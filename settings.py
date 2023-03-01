dbd3e5d86328dd53967522ff09bd5915610cb4ac
jdl456 <92414634+jdl456@users.noreply.github.com>
2023-03-01 17:48:57 -0500

    Update settings.py
    #new changes to settings.py 3/1

--- settings.py
+++ settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
 
 
 # Internationalization
